c203dec7d425e5d30178273e85c10c6e58bbd576
Sam Gottfried <sgottfrd@gmail.com>
2013-10-28 12:13:08 -0400

    Added authors names to AuthorsViewController.

--- app/controllers/authors_view_controller.rb
+++ app/controllers/authors_view_controller.rb
@@ -9,6 +9,20 @@ class AuthorsViewController < UITableViewController
 
   def viewDidLoad
     super
+    self.title = 'Authors'
     self.tableView.setSeparatorInset(UIEdgeInsetsZero)
+  end
+
+  def tableView(table_view, cellForRowAtIndexPath:index_path)
+    cell_identifier = 'author_cell'
+    cell = table_view.dequeueReusableCellWithIdentifier(cell_identifier) ||
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:cell_identifier)
+
+    cell.textLabel.text = @authors[index_path.row]
+    cell
+  end
+
+  def tableView(tableView, numberOfRowsInSection:section)
+    @authors.length
   end
 end
--- spec/controllers/authors_view_controller_spec.rb
+++ spec/controllers/authors_view_controller_spec.rb
@@ -1,12 +1,19 @@
 describe 'AuthorsViewController' do
   tests AuthorsViewController
 
+  @authors = ['author1', 'author2', 'author3']
+
   def controller
-    AuthorsViewController.alloc.initWithAuthors(['author1', 'author2', 'author3'])
+    AuthorsViewController.alloc.initWithAuthors(@authors)
   end
 
   it 'should be initialized with authors' do
     controller.authors.should != nil
   end
 
+  it 'should add the authors name for each cell' do
+    @authors.each do |author|
+      view(author).should != nil
+    end
+  end
 end
